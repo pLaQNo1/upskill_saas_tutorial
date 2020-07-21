e9239aa25f55a980d5aae213f2ff3b26621fb3e6
Stefan Frings <st.frings@gmail.com>
2020-07-21 12:53:36 +0200

    Added server side code for creating subscriptions for pro users --doesnt work--

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :plan
+
+  attr_accessor :stripe_card_token
+  def save_with_subscription
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: 'price_1H7FcRDOke6Sq7r5bL1Iia89', card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
